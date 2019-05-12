c4a6affe7b9b5f1b5aa8901de71bc1e3da77e0d3
Ruwai <chrisl6uie@gmail.com>
2019-05-11 20:06:28 -0700

    p25

--- working-with-lists/p24.scala
+++ working-with-lists/p24.scala
@@ -1,1 +1,10 @@
-http://aperiodic.net/phil/scala/s-99/p24.scala
+// P24 (*) Lotto: Draw N different random numbers from the set 1..M.
+//     Example:
+//     scala> lotto(6, 49)
+//     res0: List[Int] = List(23, 1, 17, 33, 21, 37)
+
+object P24 {
+  import P23.randomSelect
+  def lotto(count: Int, max: Int): List[Int] = 
+    randomSelect(count, List.range(1, max + 1))
+}
